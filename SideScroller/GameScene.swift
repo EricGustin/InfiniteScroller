fc079b87143a5fe9f8b49b78c96e78d51fc45292
Gustiin <ericgustin44@gmail.com>
2020-07-22 14:29:21 -0700

    Obstacles of varying height

--- SideScroller/GameScene.swift
+++ SideScroller/GameScene.swift
@@ -9,24 +9,46 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
   
   private var background = [SKSpriteNode]()
   private var player: SKSpriteNode!
+  private var playerYVelocity: Double = 0
+  private var bottomObstacles: [SKSpriteNode] = []
+  private var topObstacles: [SKSpriteNode] = []
+  
+  private var isGamePaused = false
+  
+  private var playerTextureAtlas = SKTextureAtlas()
+  private var playerTextureArray = [SKTexture]()
+  
+  private var topOfGroundY: CGFloat!
   
   override func didMove(to view: SKView) {
+    playerTextureAtlas = SKTextureAtlas(named: "playerFlying")
+    for i in 1...playerTextureAtlas.textureNames.count {
+      playerTextureArray.append(SKTexture(imageNamed: "frame-\(i).png"))
+    }
+    topOfGroundY = 6*(self.scene?.size.height)!/14
+    physicsWorld.gravity = .zero
     setUpNodes()
+    setUpGestureRecognizers()
   }
   
   override func update(_ currentTime: TimeInterval) {
-    moveGround()
-    moveBackground()
+    if !isGamePaused {
+      moveGround()
+      moveBackground()
+      moveObstacles()
+      movePlayer()
+    }
   }
   
   private func setUpNodes() {
     createBackground()
     createGround()
     createPlayer()
+    createObstacles()
   }
   
   private func createBackground() {
@@ -48,31 +70,59 @@ class GameScene: SKScene {
     for i in 0..<2 {
       let groundImage = SKSpriteNode(imageNamed: "ground@4x")
       groundImage.name = "Ground"
-      groundImage.size = CGSize(width: (self.scene?.size.width)!, height: 250)
+      groundImage.size = CGSize(width: (self.scene?.size.width)!, height: (self.scene?.size.height)!/7)
       groundImage.anchorPoint = CGPoint(x: 0.5, y: 0.5)
       groundImage.zPosition = -1
       groundImage.position = CGPoint(x: CGFloat(i)*groundImage.size.width, y: -self.frame.size.height/2)
-      
+      groundImage.physicsBody = SKPhysicsBody(rectangleOf: groundImage.size)
+      groundImage.physicsBody?.isDynamic = false
       self.addChild(groundImage)
     }
   }
   
   private func createPlayer() {
-    player = SKSpriteNode(imageNamed: "player@4x")
+    player = SKSpriteNode(imageNamed: playerTextureAtlas.textureNames[0])
     player.name = "Player"
     player.zPosition = 0
-    player.setScale(CGFloat(0.25))
+    player.setScale(CGFloat(0.2))
     player.position = CGPoint(x: -(scene?.frame.width)!/12, y: 0)
-    let transform = SKTransformNode()
-    transform.addChild(player)
+    player.physicsBody = SKPhysicsBody(circleOfRadius: player.frame.width/2)
+    player.physicsBody?.affectedByGravity = true
+    player.physicsBody?.contactTestBitMask = player.physicsBody?.collisionBitMask ?? 0
+    player.run(SKAction.repeatForever(SKAction.animate(with: playerTextureArray, timePerFrame: 0.15)))
     
-    scene?.addChild(transform)
-    transform.yRotation = 5*CGFloat.pi/12
+
+    scene?.addChild(player)
+  }
+  
+  private func createObstacles() {
+    for i in 0...3 {
+      let randomYScale = CGFloat.random(in: 0.25...0.75)
+      
+      topObstacles.append(SKSpriteNode(imageNamed: "upsideDownTube@4x"))
+      topObstacles[i].xScale = 1.75
+      topObstacles[i].size = CGSize(width: topObstacles[i].frame.width, height: (self.scene?.size.height)!)
+      topObstacles[i].yScale = 0.75 - randomYScale
+      topObstacles[i].zPosition = 0
+      topObstacles[i].name = "TopObstacle"
+      topObstacles[i].position = CGPoint(x: CGFloat(i+1)*(self.scene?.size.width)!/2, y: (self.scene?.size.height)!/2 - topObstacles[i].frame.height/2)
+      scene?.addChild(topObstacles[i])
+      
+      bottomObstacles.append(SKSpriteNode(imageNamed: "tube@4x"))
+      bottomObstacles[i].xScale = 1.75
+      bottomObstacles[i].size = CGSize(width: bottomObstacles[i].frame.width, height: (self.scene?.size.height)!)
+      bottomObstacles[i].yScale = randomYScale
+      bottomObstacles[i].zPosition = 0
+      bottomObstacles[i].name = "BottomObstacle"
+      bottomObstacles[i].position = CGPoint(x: CGFloat(i+1)*(self.scene?.size.width)!/2, y: bottomObstacles[i].size.height/2-6*(self.scene?.size.height)!/14)
+      scene?.addChild(bottomObstacles[i])
+    }
+
   }
   
   private func moveGround() {
     self.enumerateChildNodes(withName: "Ground") { (node, error) in
-      node.position.x -= 2
+      node.position.x -= 3
       if node.position.x < -(self.scene?.size.width)! {
         node.position.x += ((self.scene?.size.width)!)*2
       }
@@ -88,16 +138,80 @@ class GameScene: SKScene {
     }
   }
   
-  func touchDown(atPoint pos : CGPoint) {
+  private func moveObstacles() {
+    let random = CGFloat.random(in: 0.25...0.75)
+    self.enumerateChildNodes(withName: "BottomObstacle") { (node, error) in
+      node.position.x -= 3
+      if node.position.x < -((self.scene?.size.width)!/2) - node.frame.width/2 {
+        node.position.x = (self.scene?.size.width)!
+        node.yScale = random
+        node.position.y = node.frame.height/2-self.topOfGroundY
+      }
+    }
+    self.enumerateChildNodes(withName: "TopObstacle") { (node, error) in
+      node.position.x -= 3
+      if node.position.x < -((self.scene?.size.width)!/2) - node.frame.width/2 {
+        node.position.x = (self.scene?.size.width)!
+        node.yScale = 0.75 - random
+        node.position.y = (self.scene?.size.height)!/2 - node.frame.height/2
+      }
+    }
   }
   
-  func touchMoved(toPoint pos : CGPoint) {
+  private func movePlayer() {
+    player.position.y += CGFloat(playerYVelocity)
   }
   
-  func touchUp(atPoint pos : CGPoint) {
+  private func setUpGestureRecognizers() {
+    physicsWorld.contactDelegate = self
+    let slideUp = UISwipeGestureRecognizer(target: self, action: #selector(changeGravity))
+    slideUp.direction = .up
+    view?.addGestureRecognizer(slideUp)
+    let slideDown = UISwipeGestureRecognizer(target: self, action: #selector(changeGravity))
+    slideDown.direction = .down
+    view?.addGestureRecognizer(slideDown)
+  }
+  
+  private func collision(between player: SKNode, object: SKNode) {
+    if object.name == "Ground" {
+      pauseGame()
+    }
+  }
+  
+  private func pauseGame() {
+    isGamePaused = true
+    player.removeAllActions()
+  }
+  
+  func didBegin(_ contact: SKPhysicsContact) {
+    guard let nodeA = contact.bodyA.node else { return }
+    guard let nodeB = contact.bodyB.node else { return }
+    if nodeA.name == "Player" {
+      collision(between: nodeA, object: nodeB)
+    } else if nodeB.name == "Player" {
+      collision(between: nodeB, object: nodeA)
+    }
   }
   
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    if !isGamePaused {
+      physicsWorld.gravity = CGVector(dx: 0, dy: 0)
+      playerYVelocity = 0
+    }
+  }
+  
+  @objc func changeGravity(_ sender: UISwipeGestureRecognizer) {
+    if !isGamePaused {
+      guard sender.view != nil else { return }
+      if sender.direction == .up {
+        print("up")
+        playerYVelocity = 8.0
+      }
+      if sender.direction == .down {
+        print("down")
+        playerYVelocity = -8.0
+      }
+    }
   }
   
   override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -108,4 +222,6 @@ class GameScene: SKScene {
   
   override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
   }
+
+  
 }
